87786e9703509aa2ff9ee960eab33d9555f1a2d8
waiting <1661926154@qq.com>
2020-02-27 17:02:54 +0800

    chore(demo-service): output pkgName and pkgVersion during starting

--- packages/demo-service/src/app.ts
+++ packages/demo-service/src/app.ts
@@ -7,6 +7,12 @@ export default (app: EggApplication) => {
   app.beforeStart(async () => {
     console.log('🚀 Your awesome APP is launching...')
 
+    const { pkg } = app.config
+    console.info({
+      pkgName: pkg.name,
+      pkgVersion: pkg.version,
+    })
+
     console.log('✅  Your awesome APP launched')
   })
 }
