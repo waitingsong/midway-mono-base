175ff658ad389bf0e83dba31dedd5399af2fe48e
waiting <1661926154@qq.com>
2021-11-11 17:58:49 +0800

    chore: update config and bump deps version

--- packages/demo-docs/src/dto/auth.dto.ts
+++ packages/demo-docs/src/dto/auth.dto.ts
@@ -4,12 +4,10 @@ import { userValidSchemas } from '../validation-schema/index.schema'
 
 
 export class LoginDTO {
-
   @Rule(userValidSchemas.uname.required())
   username: string
 
   @Rule(userValidSchemas.hashpwd.required())
   hashpwd: string
-
 }
 
