d0aea130faf37c9708b0db24c76b4fcaf65caaf6
waiting <1661926154@qq.com>
2021-11-17 10:17:50 +0800

    ci: update scripts

--- .scripts/ci/ci-ssh-agent.sh
+++ .scripts/ci/ci-ssh-agent.sh
@@ -19,8 +19,10 @@ chmod 700 ~/.ssh
 which ssh-agent
 eval $(ssh-agent -s)
 
-ssh-keyscan -t ed25519 -p $CI_SSH_PORT $CI_SERVER_HOST >> ~/.ssh/known_hosts
-cat ~/.ssh/known_hosts
+if [ ! -f ~/.ssh/known_hosts ]; then
+  ssh-keyscan -t ed25519 -p $CI_SSH_PORT $CI_SERVER_HOST >> ~/.ssh/known_hosts
+  cat ~/.ssh/known_hosts
+fi
 
 # echo "$CI_SSH_PRIVATE_KEY" | base64 -d -
 echo "$CI_SSH_PRIVATE_KEY" | base64 -d - | tr -d '\r' | ssh-add - > /dev/null
