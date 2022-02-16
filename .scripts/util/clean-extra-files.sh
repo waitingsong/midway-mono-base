a790dc495ca1528667489ff863f12e9f0bd52629
waiting <1661926154@qq.com>
2022-02-16 13:32:56 +0800

    ci: update clean scripts

--- .scripts/util/clean-extra-files.sh
+++ .scripts/util/clean-extra-files.sh
@@ -5,53 +5,53 @@
 
 echo -e ">>> Purging extra files"
 
-find $(find . -type d -iname "@mw-components") -maxdepth 3 -type d -iname "database" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "@mw-components") -maxdepth 3 -type d -iname "database" -print0 | xargs -0II rm -rf I
 
 find $(find $(find . -type d -iname "@waiting") -mindepth 2 -maxdepth 2 -type d -name "dist") \
-  -maxdepth 1 -type f -iname "*.esm.*" -print0 | xargs -0i rm -f {}
+  -maxdepth 1 -type f -iname "*.esm.*" -print0 | xargs -0II rm -f I
 find $(find $(find . -type d -iname "@waiting") -mindepth 2 -maxdepth 2 -type d -name "dist") \
-  -maxdepth 1 -type f -iname "*.umd.*" -print0 | xargs -0i rm -f {}
+  -maxdepth 1 -type f -iname "*.umd.*" -print0 | xargs -0II rm -f I
 
-find $(find . -type d -iname "ajv") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "ajv") -maxdepth 1 -type d -iname "scripts" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "ajv") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "ajv") -maxdepth 1 -type d -iname "scripts" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "ali-oss") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "ali-oss") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "joi") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "joi") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "json5") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "json5") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "moment") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "moment") -maxdepth 1 -type d -iname "src" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "moment") -maxdepth 1 -type d -iname "min" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "moment") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "moment") -maxdepth 1 -type d -iname "src" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "moment") -maxdepth 1 -type d -iname "min" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "moment-timezone") -maxdepth 1 -type d -iname "builds" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "moment-timezone") -maxdepth 1 -type d -iname "builds" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "nunjucks") -maxdepth 1 -type d -iname "browser" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "nunjucks") -maxdepth 1 -type d -iname "browser" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "needle") -maxdepth 1 -type d -iname "examples" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "needle") -maxdepth 1 -type d -iname "examples" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "opentracing") -maxdepth 1 -type d -iname "src" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "opentracing") -maxdepth 1 -type d -iname "src" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "src" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "_esm5" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "_esm2015" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "add" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "migrations" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "observable" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "operator" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "testing" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "src" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "_esm5" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "_esm2015" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "add" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "migrations" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "observable" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "operator" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 1 -type d -iname "testing" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "bundles" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "esm5" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "esm" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "types" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "bundles" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "esm5" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "esm" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "rxjs") -maxdepth 2 -type d -iname "types" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "sqlite3") -maxdepth 1 -type d -iname "src" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "sqlite3") -maxdepth 1 -type d -iname "deps" -print0 | xargs -0i rm -rf {}
-find $(find . -type d -iname "sqlite3") -maxdepth 1 -type d -iname "tools" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "sqlite3") -maxdepth 1 -type d -iname "src" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "sqlite3") -maxdepth 1 -type d -iname "deps" -print0 | xargs -0II rm -rf I
+find $(find . -type d -iname "sqlite3") -maxdepth 1 -type d -iname "tools" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "source-map") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "source-map") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
 
-find $(find . -type d -iname "winston") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0i rm -rf {}
+find $(find . -type d -iname "winston") -maxdepth 1 -type d -iname "dist" -print0 | xargs -0II rm -rf I
 
--- .scripts/util/clean-pkg-files.sh
+++ .scripts/util/clean-pkg-files.sh
@@ -4,61 +4,61 @@
 set -e
 
 
-find . -mindepth 3 -type d -iname "docs" -print0 | xargs -0i rm -rf {}
+find . -mindepth 3 -type d -iname "docs" -print0 | xargs -P0 -0II rm -rf I
 
-find . -type d -iname "@types" -print0 | xargs -0i rm -rf {}
-find . -type d -iname ".github" -print0 | xargs -0i rm -rf {}
-find . -type d -iname "benchmarks" -print0 | xargs -0i rm -rf {}
-find . -type d -iname "example" -print0 | xargs -0i rm -rf {}
-find . -type d -iname "man1" -print0 | xargs -0i rm -rf {}
-find . -type d -iname "spec" -print0 | xargs -0i rm -rf {}
-find . -type d -iname "test" -print0 | xargs -0i rm -rf {}
-find . -type d -iname "tests" -print0 | xargs -0i rm -rf {}
+find . -type d -iname "@types" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname ".github" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname "benchmarks" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname "example" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname "man1" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname "spec" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname "test" -print0 | xargs -P0 -0II rm -rf I
+find . -type d -iname "tests" -print0 | xargs -P0 -0II rm -rf I
 
 # unlink for image build cache
-find . -type f -iname "package-lock.json" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".package-lock.json" -print0 | xargs -0i rm -f {}
+find . -type f -iname "package-lock.json" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".package-lock.json" -print0 | xargs -P0 -0II rm -f I
 
-find . -type f -iname ".bak" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".coveralls.yml" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".DS_Store" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".dockerignore" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".editorconfig" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".eslint*" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".gitattributes" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".github" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".istanbul.yml" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".jshintrc" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".jscs.json" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".jscsrc" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".name" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".npmignore" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".nycrc" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".prettierrc*" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".travis.yml" -print0 | xargs -0i rm -f {}
-find . -type f -iname ".tslint*" -print0 | xargs -0i rm -f {}
-find . -type f -iname "appveyor.yml" -print0 | xargs -0i rm -f {}
-find . -type f -iname "docker-compose.yml" -print0 | xargs -0i rm -f {}
-find . -type f -iname "Dockerfile" -print0 | xargs -0i rm -f {}
-find . -type f -iname "donate.png" -print0 | xargs -0i rm -f {}
-find . -type f -iname "example.png" -print0 | xargs -0i rm -f {}
-find . -type f -iname "flake.lock" -print0 | xargs -0i rm -f {}
-find . -type f -iname "float.patch" -print0 | xargs -0i rm -f {}
-find . -type f -iname "CHANGELOG*" -print0 | xargs -0i rm -rf {}
-find . -type f -iname "CHANGELOG.rst" -print0 | xargs -0i rm -rf {}
-find . -type f -iname "Makefile" -print0 | xargs -0i rm -f {}
-find . -type f -iname "test.js" -print0 | xargs -0i rm -f {}
-find . -type f -iname "yarn.lock" -print0 | xargs -0i rm -f {}
-find . -type f -iname "*.markdown" -print0 | xargs -0i rm -f {}
-find . -type f -iname "*.md" -print0 | xargs -0i rm -f {}
-find . -type f -iname "*.swp" -print0 | xargs -0i rm -f {}
+find . -type f -iname ".bak" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".coveralls.yml" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".DS_Store" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".dockerignore" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".editorconfig" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".eslint*" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".gitattributes" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".github" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".istanbul.yml" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".jshintrc" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".jscs.json" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".jscsrc" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".name" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".npmignore" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".nycrc" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".prettierrc*" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".travis.yml" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname ".tslint*" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "appveyor.yml" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "docker-compose.yml" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "Dockerfile" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "donate.png" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "example.png" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "flake.lock" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "float.patch" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "CHANGELOG*" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "CHANGELOG.rst" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "Makefile" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "test.js" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "yarn.lock" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "*.markdown" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "*.md" -print0 | xargs -P0 -0II rm -f I
+find . -type f -iname "*.swp" -print0 | xargs -P0 -0II rm -f I
 
-find . -type f -iname "*.html" -not -path "./koa-onerror/templates/*" -print0 | xargs -0i rm -f {}
+find . -type f -iname "*.html" -not -path "./koa-onerror/templates/*" -print0 | xargs -P0 -0II rm -f I
 
-find . -type f -iname "*.d.ts" -print0 | xargs -0i rm -f {}
+find . -type f -iname "*.d.ts" -print0 | xargs -P0 -0II rm -f I
 
-find . -type f -iname "LICENCE*" -print0 | xargs -0i gzip {}
-find . -type f -iname "LICENSE*" -print0 | xargs -0i gzip {}
+find . -type f -iname "LICENCE*" -print0 | xargs -P0 -0II gzip I
+find . -type f -iname "LICENSE*" -print0 | xargs -P0 -0II gzip I
 
 cd ../
 
