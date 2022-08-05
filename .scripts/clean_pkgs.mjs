13df92363612c38ab6ae3d0e26a53c268057c385
waiting <1661926154@qq.com>
2022-08-05 18:06:34 +0800

    chore(boilerplate): bump deps version

---
+++ .scripts/clean_pkgs.mjs
@@ -0,0 +1,9 @@
+#!/usr/bin/env zx
+
+
+await $`rm -rf node_modules/@midwayjs/cli-plugin-build/node_modules/typescript/ `
+await $`rm -rf node_modules/@midwayjs/cli-plugin-test/node_modules/ `
+await $`rm -rf node_modules/dependency-tree/node_modules/typescript/ `
+await $`rm -rf node_modules/detective-typescript/node_modules/typescript/`
+await $`rm -rf node_modules/filing-cabinet/node_modules/typescript/ `
+await $`rm -rf node_modules/madge/node_modules/typescript/ `
