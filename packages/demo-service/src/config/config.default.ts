ea7c46ca8d6c6cbcf68f82f37fcf0b97718ccde4
waiting <1661926154@qq.com>
2022-08-17 17:45:33 +0800

    chore(boilerplate): update

--- packages/demo-service/src/config/config.default.ts
+++ packages/demo-service/src/config/config.default.ts
@@ -1,42 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { MidwayAppInfo, MidwayConfig } from '@midwayjs/core'
-import type { NpmPkg } from '@mw-components/share'
-import { retrieveFirstIp } from '@waiting/shared-core'
 
 
-export default (appInfo: MidwayAppInfo): MidwayConfig => {
+export const welcomeMsg = 'Hello Midwayjs!'
 
-  const config = {} as MidwayConfig
+export const keys = '1559532739677_8888'
 
-  config.koa = {
-    port: 7001,
-  }
-
-  config.welcomeMsg = 'Hello Midwayjs!'
-
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = '1559532739677_8888'
-
-  // '2020-01-01T00:00:00Z'
-  const epoch = 1577836800000
-  config.koidConfig = {
-    node: 0,
-    epoch,
-  }
-
-  const ip = retrieveFirstIp()
-  const nameNorm = (appInfo.pkg as NpmPkg).name.replace(/@/ug, '').replace(/\//ug, '-')
-  config.prometheus = {
-    labels: {
-      APP_NAME: (appInfo.pkg as NpmPkg).name,
-      APP_NAME_NORM: nameNorm,
-      APP_VER: (appInfo.pkg as NpmPkg).version,
-      APP_PID: process.pid,
-      APP_PPID: process.ppid,
-      APP_IPs: ip ? ip.address : 'n/a',
-    },
-  }
-
-
-  return config
-}
--- packages/demo-service/src/types.ts
+++ packages/demo-service/src/types.ts
@@ -11,14 +11,3 @@ export interface JwtUser {
   uid: string
 }
 
-
-export interface AppInfomation {
-  pkgName: string
-  pkgVer: string
-  pid: number
-  ppid: number
-  ip: string
-  reqId: string
-  [key: string]: string | number
-}
-
