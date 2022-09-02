d9d1e3fbaa92cb89b88cbe0b19c284a797a7586e
waiting <1661926154@qq.com>
2022-09-02 10:25:33 +0800

    chore(boilerplate): benchmark

--- packages/demo-docs/rollup.config.js
+++ packages/demo-docs/rollup.config.js
@@ -1,4 +1,4 @@
-import { basename } from 'path'
+import { basename } from 'node:path'
 // import commonjs from '@rollup/plugin-commonjs'
 // import resolve from '@rollup/plugin-node-resolve'
 // import { terser } from 'rollup-plugin-terser'
