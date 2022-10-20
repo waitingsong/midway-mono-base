7555e4efc8ac05d45390ae92961f604d3c8f6d81
waiting <1661926154@qq.com>
2022-10-20 17:22:28 +0800

    ci: update pack config

--- .scripts/ci/ci-pkg-prepack.sh
+++ .scripts/ci/ci-pkg-prepack.sh
@@ -6,11 +6,11 @@ echo '============================== ci-pkg-prepack ==========================='
 
 if [ -n "$NPM_REGISTRY" ]; then
   echo -e "\$NPM_REGISTRY: $NPM_REGISTRY"
-  rm node_modules -rf
+  # rm node_modules -rf
   npm i --no-audit --omit=dev --omit=optional --legacy-peer-deps --ignore-scripts  --package-lock-only \
     --registry "$NPM_REGISTRY"
 else
-  rm node_modules -rf
+  # rm node_modules -rf
   npm i --no-audit --omit=dev --omit=optional --legacy-peer-deps --ignore-scripts  --package-lock-only
 fi
 
