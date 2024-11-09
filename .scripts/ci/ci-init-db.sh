eb28ea28034009e4bf2a4ebf4a4aedadf1d43ec3
waiting <1661926154@qq.com>
2024-11-09 21:39:21 +0800

    ci: update script

--- .scripts/ci/ci-init-db.sh
+++ .scripts/ci/ci-init-db.sh
@@ -7,11 +7,13 @@ dig postgres
 
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SELECT version();"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SHOW TIMEZONE;"
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SELECT extname, extversion FROM pg_extension;"
 
 echo -e "\n"
 
 psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SELECT version();"
 psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SHOW TIMEZONE;"
+psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SELECT extname, extversion FROM pg_extension;"
 
 SQL_DIR='./node_modules/@waiting/pgmq-js/database/'
 cd "$SQL_DIR"
